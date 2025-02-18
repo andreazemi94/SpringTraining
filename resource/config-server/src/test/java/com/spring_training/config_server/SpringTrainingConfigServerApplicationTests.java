0ce95793e2b6c20b8725c40836137a19c56c8e3f
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-02-18 23:26:57 +0100

    create Spring Training Config Server Service

---
+++ resource/config-server/src/test/java/com/spring_training/config_server/SpringTrainingConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_training.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTrainingConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
