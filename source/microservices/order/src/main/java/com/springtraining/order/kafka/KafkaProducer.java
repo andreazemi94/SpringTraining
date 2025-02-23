1a38f6ba8ec90e0f37c3498d83f256c22ffcc3a4
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-02-23 14:30:55 +0100

    add producer kafka

--- source/microservices/order/src/main/java/com/springtraining/order/kafka/KafkaProducer.java
+++ source/microservices/order/src/main/java/com/springtraining/order/kafka/KafkaProducer.java
@@ -12,18 +12,26 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Objects;
+
 @RequiredArgsConstructor
 @Service
 @Slf4j
 public class KafkaProducer {
     @Value("${spring.kafka.topic.order}")
     private String orderTopic;
-    private final KafkaTemplate<Long, SpecificRecord> kafkaTemplate;
+    private final KafkaTemplate<String, OrderDto> kafkaTemplate;
     private final ModelMapper modelMapper;
 
     public void sendMessage(Order order){
         OrderDto orderDto = modelMapper.map(order,OrderDto.class);
-        ProducerRecord<Long,SpecificRecord> record = new ProducerRecord<>(orderTopic, order.getOrderId(), orderDto);
-        kafkaTemplate.send(record).thenAccept(sendResult->log.info("Sended message to topic {}, {}", orderTopic, order));
+        ProducerRecord<String,OrderDto> record = new ProducerRecord<>(orderTopic, order.getOrderId().toString(), orderDto);
+        kafkaTemplate.send(record)
+                .whenCompleteAsync((sentResult,exception)->{
+                    if (Objects.nonNull(exception))
+                        log.error(exception.getMessage());
+                    else
+                        log.info("Sended message to topic {}, {}", orderTopic, order);
+                });
     }
 }
