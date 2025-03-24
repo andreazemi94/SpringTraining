e39f290ba2bfd03e76fad804ff9abe355a46165d
Andrea Zemignani <andrea.zemignani@acsoftware.it>
2025-03-24 10:01:31 +0100

    add refresh scope

--- source/microservices/order/src/main/java/com/springtraining/order/config/SchemaRegistryClientConfig.java
+++ source/microservices/order/src/main/java/com/springtraining/order/config/SchemaRegistryClientConfig.java
@@ -3,10 +3,12 @@ package com.springtraining.order.config;
 import io.confluent.kafka.schemaregistry.client.CachedSchemaRegistryClient;
 import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RefreshScope
 public class SchemaRegistryClientConfig {
 
     @Value("${spring.kafka.properties.schema.registry.url}")
--- source/microservices/order/src/main/java/com/springtraining/order/kafka/KafkaProducer.java
+++ source/microservices/order/src/main/java/com/springtraining/order/kafka/KafkaProducer.java
@@ -7,12 +7,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
 @Service
 @Slf4j
+@RefreshScope
 public class KafkaProducer {
     @Value("${spring.kafka.topic.order}")
     private String orderTopic;
